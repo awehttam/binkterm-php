780c017ee06420a4cfe3200b66aa474ae71c596a
awehttam <awehttam@gmail.com>
2026-01-28 11:36:17 -0800

    Fix Windows daemon status and layout

---
+++ src/SystemStatus.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BinktermPHP;
+
+class SystemStatus
+{
+    public static function getDaemonStatus(): array
+    {
+        $runDir = __DIR__ . '/../data/run';
+        $pidFiles = [
+            'admin_daemon' => $runDir . '/admin_daemon.pid',
+            'binkp_scheduler' => $runDir . '/binkp_scheduler.pid',
+            'binkp_server' => $runDir . '/binkp_server.pid'
+        ];
+
+        $status = [];
+
+        foreach ($pidFiles as $name => $pidFile) {
+            $pid = null;
+            $running = false;
+
+            if (file_exists($pidFile)) {
+                $pid = trim(file_get_contents($pidFile));
+                if ($pid !== '' && is_numeric($pid)) {
+                    $pidInt = (int)$pid;
+                    if (function_exists('posix_kill')) {
+                        $running = @posix_kill($pidInt, 0);
+                    } else {
+                        $running = self::isProcessRunningWindows($pidInt);
+                    }
+                }
+            }
+
+            $status[$name] = [
+                'pid_file' => $pidFile,
+                'pid' => $pid ?: null,
+                'running' => $running
+            ];
+        }
+
+        return $status;
+    }
+
+    private static function isProcessRunningWindows(int $pid): bool
+    {
+        $output = [];
+        $cmd = 'tasklist /FI "PID eq ' . $pid . '" /NH';
+        @exec($cmd, $output);
+        if (empty($output)) {
+            return false;
+        }
+
+        foreach ($output as $line) {
+            if ($line === '' || stripos($line, 'INFO:') === 0) {
+                continue;
+            }
+            if (preg_match('/\b' . preg_quote((string)$pid, '/') . '\b/', $line)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
