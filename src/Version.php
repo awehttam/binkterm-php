0c3850e3c8be866c37106f906bfaf312bf6416f3
awehttam <awehttam@gmail.com>
2026-01-30 15:33:47 -0800

    Bump version to 1.7.3

--- src/Version.php
+++ src/Version.php
@@ -17,7 +17,7 @@ class Version
      * This should be updated when releasing new versions.
      * Format: MAJOR.MINOR.PATCH
      */
-    private const VERSION = '1.7.2';
+    private const VERSION = '1.7.3';
     
     /**
      * Get the current application version
