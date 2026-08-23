573f81e320cec64488156a22a4e73b2c43542a93
root <root@L33Test.com>
2026-08-23 01:19:09 -0600

    Pause after L33TEST ANSI logo splash

--- telnet/src/BbsSession.php
+++ telnet/src/BbsSession.php
@@ -2137,7 +2137,16 @@ class BbsSession
             return;
         }
 
-        TelnetUtils::showScreenIfExists("l33test-logo.ans", $this, $conn);
+        if (TelnetUtils::showScreenIfExists("l33test-logo.ans", $this, $conn)) {
+            $this->safeWrite($conn, "\r\nPress any key to continue...");
+
+            while (true) {
+                $key = $this->readKeyWithIdleCheck($conn, $state);
+                if ($key !== null && $key !== '') {
+                    break;
+                }
+            }
+        }
 
         if (TelnetUtils::showScreenIfExists("login.ans", $this, $conn)) {
             return;
