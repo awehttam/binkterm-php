5ff547c5424335f889bad7c1c85cc00d718b065b
root <root@L33Test.com>
2026-08-23 00:01:44 -0600

    Update BBSLink native door assets

---
+++ native-doors/doors/bbslinknative/vars.sh
@@ -0,0 +1,7 @@
+# Copy this file to vars.sh, and adjust the settings below to match your
+# configuration
+
+host="games.bbslink.net" 		# Server address, usually 'games.bbslink.net'
+syscode="l33test"		# Your system code
+authcode="gMjIaMdvoUhw"		# Your system's authorisation code
+schemecode="RXy8MhGaPw9k"		# Scheme code
